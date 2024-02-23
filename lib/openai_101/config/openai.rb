3203aec3a21a51f97bb08fdb5582f3365d5a8e33
David Cruwys <david@ideasmen.com.au>
2024-02-23 15:36:45 +1000

    fix: handle openai access token in CI

--- lib/openai_101/config/openai.rb
+++ lib/openai_101/config/openai.rb
@@ -5,6 +5,11 @@ require 'dotenv'
 Dotenv.load('.env')
 
 OpenAI.configure do |config|
-  config.access_token = ENV.fetch('OPENAI_ACCESS_TOKEN')
-  config.organization_id = ENV.fetch('OPENAI_ORGANIZATION_ID', nil)
+  tools_enabled = ENV.fetch('TOOLS_ENABLED', 'false')
+
+  if tools_enabled == 'true'
+    puts 'OpenAI Tools are enabled'
+    config.access_token = ENV.fetch('OPENAI_ACCESS_TOKEN')
+    config.organization_id = ENV.fetch('OPENAI_ORGANIZATION_ID', nil)
+  end
 end
